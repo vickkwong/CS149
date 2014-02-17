b47a3e6eaec012e83cb2d414df2b83aac67abb85
Victoria Kwong <vickkwong@gmail.com>
2014-02-17 14:52:48 -0800

    Map and Reduce methods - rough

--- pa3/Ngram.java
+++ pa3/Ngram.java
@@ -1,8 +1,15 @@
 package cs149.ngram;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
@@ -24,10 +31,9 @@ import org.apache.hadoop.util.ToolRunner;
 
 public class Ngram extends Configured implements Tool {
 
-	/**
-	 * @param args
-	 * @throws Exception 
-	 */
+	public static int n;
+	public static String queryFile;
+	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println(Arrays.toString(args));
@@ -39,6 +45,8 @@ public class Ngram extends Configured implements Tool {
 	public int run(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println(Arrays.toString(args));
+		n = Integer.parseInt(args[0]);
+		queryFile = args[1];
 		JobConf job = new JobConf(Ngram.class);
 		job.setJobName("Ngram");
 		job.setOutputKeyClass(Text.class);
@@ -51,9 +59,8 @@ public class Ngram extends Configured implements Tool {
 		job.setInputFormat(CustomInputFormat.class);
 		job.setOutputFormat(TextOutputFormat.class);
 
-		Path tempPath = new Path("temp");
-		FileInputFormat.setInputPaths(job, new Path("wikipedia/8gb"));
-		FileOutputFormat.setOutputPath(job, tempPath);
+		FileInputFormat.setInputPaths(job, new Path(args[2]));
+		FileOutputFormat.setOutputPath(job, new Path(args[3]));
 
 		JobClient.runJob(job);
 		
@@ -62,10 +69,28 @@ public class Ngram extends Configured implements Tool {
 	
 	public static class Map extends MapReduceBase implements Mapper<Text, Text, Text, IntWritable> {
 
+		private final static IntWritable ONE = new IntWritable(1);
+		private Set<String> queryNgrams = new HashSet<String>();
+		
 		@Override
 		public void configure(JobConf arg0) {
 			// TODO Auto-generated method stub
-			
+			try {
+				Scanner scanner = new Scanner(new File(queryFile)).useDelimiter("\\Z");
+				String queryContents = scanner.next();
+				Tokenizer tokenizer = new Tokenizer(queryContents);
+				Queue<String> ngram = new LinkedList<String>();
+				while (tokenizer.hasNext()) {
+					ngram.add(tokenizer.next());
+					if (ngram.size() == n) {
+						String ngramValue = ngram.toString().replace(",", "").substring(1, ngram.toString().length() - n);
+						queryNgrams.add(ngramValue);
+					}
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		@Override
@@ -75,11 +100,19 @@ public class Ngram extends Configured implements Tool {
 		}
 
 		@Override
-		public void map(Text arg0, Text arg1,
-				OutputCollector<Text, IntWritable> arg2, Reporter arg3)
+		public void map(Text key, Text value,
+				OutputCollector<Text, IntWritable> output, Reporter arg3)
 				throws IOException {
-			//System.out.println("arg0:" + arg0.toString());
-			//System.out.println("arg1: " + arg1.toString());
+			Tokenizer tokenizer = new Tokenizer(value.toString());
+			Queue<String> ngram = new LinkedList<String>();
+			while (tokenizer.hasNext()) {
+				ngram.add(tokenizer.next());
+				if (ngram.size() == n) {
+					String ngramValue = ngram.toString().replace(",", "").substring(1, ngram.toString().length() - n);
+					if (queryNgrams.contains(ngramValue))
+						output.collect(key, ONE);
+				}
+			}
 			
 		}
 	}
@@ -87,11 +120,16 @@ public class Ngram extends Configured implements Tool {
 	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
 
 		@Override
-		public void reduce(Text arg0, Iterator<IntWritable> arg1,
-				OutputCollector<Text, IntWritable> arg2, Reporter arg3)
+		public void reduce(Text key, Iterator<IntWritable> vals,
+				OutputCollector<Text, IntWritable> output, Reporter arg3)
 				throws IOException {
 			// TODO Auto-generated method stub
-			
+			int sum = 0;
+			while (vals.hasNext()) {
+				IntWritable val = vals.next();
+				sum += val.get();
+			}
+			output.collect(key, new IntWritable(sum));
 		}
 		
 	}
